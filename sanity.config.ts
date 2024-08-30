e5b4713afe033413ec4973815e408d1c85040ead
dalena <dalenaxtran@gmail.com>
2024-08-30 16:31:19 -0700

    removed token from sanity.congif.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -9,14 +9,13 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId, token } from "./sanity/env";
+import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schema";
 
 export default defineConfig({
   basePath: "/studio",
   projectId,
   dataset,
-  token,
   schema,// Add and edit the content schema in the './sanity/schema' folder
   plugins: [
     structureTool(),
